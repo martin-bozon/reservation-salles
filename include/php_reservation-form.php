dc98c06af21d20211264f8cc80af87fdf096d192
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-06-10 09:53:11 +0200

    select horaire

--- include/php_reservation-form.php
+++ include/php_reservation-form.php
@@ -4,7 +4,8 @@
             header("Location:index.php");
         }
     else
-        {                              
+        {     
+            var_dump($_POST);                         
             if(isset($_POST["validresa"]) && !empty($_POST["titre"]) && !empty($_POST["description"]) && !empty($_POST["debut_date"]) && !empty($_POST["debut_heure"]) && !empty($_POST["fin_date"]) && !empty($_POST["fin_heure"]))
                 {                    
                     $debut = $_POST["debut_date"] . " " . $_POST["debut_heure"];
@@ -12,22 +13,22 @@
                     $titre = $_POST["titre"];
                     $description = $_POST["description"];        
                     $debut_str = strtotime($debut);
-                    $fin_str = strtotime($fin);    
-                    
-                    $time= explode(':', $_POST["debut_heure"]);
-                    var_dump($time);
-                    echo $time[1];//vérifie que minute = 0
+                    $fin_str = strtotime($fin);                        
+                               
+                    // // $time= explode(':', $_POST["debut_heure"]);
+                    // var_dump($time);
+                    // echo $time[1];//vérifie que minute = 0
 
-                    $sous = $fin_str - $debut_str;
-                    echo $sous; //soustraction pour vérifier créneau d'une heure
+                    // $sous = $fin_str - $debut_str;
+                    // echo $sous; //soustraction pour vérifier créneau d'une heure
                     
                     //vérifie que l'heure de début choisie n'est pas déjà enregistré
                     // $requete_creneau = "SELECT * FROM reservations WHERE debut='$debut'";
                     // $query_creneau = mysqli_query($connexionbd, $requete_creneau);
                     // $info_creneau = mysqli_fetch_all($query_creneau, MYSQLI_ASSOC);
 
-                    // if(empty($info_creneau))
-                    //     {
+                     if(empty($info_creneau))
+                         {
                              if($debut_str<time()) 
                                 //check date saisie pour début n'est pas déjà passée 
                                 //time -2 heures !!!!!                            
@@ -37,21 +38,21 @@
                             else
                                 {
                                     if($fin_str<$debut_str) 
-                                    //check si date de fin n'est pas avant début
-                                    //manque faire en sorte que le créneau dure 1h !!!!
+                            //         //check si date de fin n'est pas avant début
+                            //         //manque faire en sorte que le créneau dure 1h !!!!
                                         {
                                             echo "fin";
-                                        }
-                                    // else if(//crénau d'une heure !)
-                                    //     {
+                                       }
+                                    else if(//crénau d'une heure !)
+                                        {
                                             //ajout dans la bdd
-                                    //     }
-                                    // else
-                                    //     {
-                                    //         
-                                    //     }
+                                        }
+                                    else
+                                        {
+                                            
+                                        }
                                 }
-                            //}                                        
+                            }                                        
                 }
         }
 ?>
--- reservation-form.php
+++ reservation-form.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php 
+    session_start();
+    include 'include/php_reservation-form.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,24 +14,50 @@
 
     <main>
         <h1>Réserver une salle</h1>
-        <form action="reservation-form" method="POST">
+        <form action="reservation-form.php" method="POST">
             <label for="titre">Titre :</label>
-            <input type="text" id="titre" name="titre"/>
+            <input type="text" id="titre" name="titre" required>
 
             <label for="description">Description:</label>
-            <input type="text" id="description" name="description">
+            <input type="text" id="description" name="description" required>
 
             <label for="debut">Date de début :</label>
-            <input type="date" id="debut" name="debut_date">
-            <input type="time" id="debut" name="debut_heure">
-            <small>Première session à 8h, dernière session à 19h</small>
+            <input type="date" id="debut" name="debut_date" required>        
+            <select id="debut" name="debut_heure">
+                <option value="08:00">08:00</option>
+                <option value="09:00">09:00</option>
+                <option value="10:00">10:00</option>
+                <option value="11:00">11:00</option>
+                <option value="12:00">12:00</option>
+                <option value="13:00">13:00</option>
+                <option value="14:00">14:00</option>
+                <option value="15:00">15:00</option>
+                <option value="16:00">16:00</option>
+                <option value="17:00">17:00</option>
+                <option value="18:00">18:00</option>
+                <option value="19:00">19:00</option> 
+            </select>                       
 
-            <label for="fin">Date de début :</label>
-            <input type="date" id="fin" name="fin_date">
-            <input type="time" id="fin" name="fin_heure">
-            <small>Crénaux d'une heure !</small>
+            <label for="fin">Date de fin :</label>
+            <input type="date" id="fin" name="fin_date" required>
+            <select id="debut" name="fin_heure">
+                <option value="09:00">09:00</option>
+                <option value="10:00">10:00</option>
+                <option value="11:00">11:00</option>
+                <option value="12:00">12:00</option>
+                <option value="13:00">13:00</option>
+                <option value="14:00">14:00</option>
+                <option value="15:00">15:00</option>
+                <option value="16:00">16:00</option>
+                <option value="17:00">17:00</option>
+                <option value="18:00">18:00</option>
+                <option value="19:00">19:00</option>
+                <option value="20:00">20:00</option> 
+            </select>  
+            <small>Créneau d'une heure !</small>
+           
 
-            <input type="submit" name="validinsc" value="Inscription">
+            <input type="submit" name="validresa" value="Réserver">
         </form>
     </main>
 
