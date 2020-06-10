c654c1c0e995d191e680ae689458386b88e18d2d
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-06-10 11:21:04 +0200

    time mis à jour dans serveur wamp

--- include/php_reservation-form.php
+++ include/php_reservation-form.php
@@ -4,9 +4,7 @@
             header("Location:index.php");
         }
     else
-        {     
-            // echo time();
-            echo date("d, m ,Y, H:i");
+        {                 
             if(isset($_POST["validresa"]) && !empty($_POST["titre"]) && !empty($_POST["description"]) && !empty($_POST["debut_date"]) && !empty($_POST["debut_heure"]) && !empty($_POST["fin_date"]) && !empty($_POST["fin_heure"]))
                 {                    
                     $debut = $_POST["debut_date"] . " " . $_POST["debut_heure"];
